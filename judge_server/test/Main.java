59f4d5d319e8295916a4d041354dc292027a69e8
wyang <boywyang@gmail.com>
2013-09-10 13:54:38 +0800

    add test file

--- judge_server/test/Main.java
+++ judge_server/test/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class Sum {
 
 public static void main(String[] args) {
   Scanner scan = new Scanner(System.in);
---
+++ judge_server/test/Main.pas
@@ -0,0 +1,8 @@
+program Main(Input,Output);
+
+var
+  a,b:Integer;
+begin
+   Readln(a,b);
+   Writeln(a+b);
+end.
